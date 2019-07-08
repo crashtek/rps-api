12a380536532414ff1850a7e26748ed21de7db35
josiemostek <josiemostek@gmail.com>
2019-07-08 13:05:43 -0500

    duel has now been written to the database

--- src/service/DuelService.js
+++ src/service/DuelService.js
@@ -1,10 +1,12 @@
 import uuid from 'uuid';
+import mongo from 'mongodb';
 import logger from '../logger';
 import MessageQueueService from './MessageQueueService';
 
 class DuelService {
   constructor() {
     this.message = 'not implemented';
+    this.db = null;
   }
 
   async find(id) {
@@ -28,12 +30,24 @@ class DuelService {
 
   async create(opponent1, opponent2) {
     // create a new duel in the data base
+    const db = await this.getDb();
     logger.info(`${this.message}`);
-    // return the duel to the user
-    return {
-      id: 123456,
+    const duel = {
+      id: uuid.v4(),
       warriors: [opponent1, opponent2]
     };
+    await db.insertOne(duel);
+    // return the duel to the user
+    return duel;
+  }
+
+  async getDb() {
+    if (this.db !== null) return this.db;
+
+    const client = await mongo.MongoClient.connect(process.env.MONGO_URI);
+    const db = await client.db('rps');
+    this.db = await db.collection('duels');
+    return this.db;
   }
 }
 
